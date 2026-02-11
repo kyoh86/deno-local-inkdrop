b45250049489b1005eeeced09a33d876522c27cd
kyoh86 <me@kyoh86.dev>
2026-02-12 02:35:17 +0900

    fix: remove invalid test

--- main_test.ts
+++ main_test.ts
@@ -1,31 +1,9 @@
-import { assertEquals, assertThrows } from "@std/assert";
-import {
-  basicAuthHeader,
-  DocsAPI,
-  InkdropClient,
-  type NoteDoc,
-  NotesAPI,
-} from "./mod.ts";
+import { assertEquals } from "@std/assert";
+import { basicAuthHeader } from "./mod.ts";
 
 Deno.test(function basicAuthHeaderTest() {
   assertEquals(
     basicAuthHeader("user", "pass"),
     "Basic dXNlcjpwYXNz",
-  );
-  assertThrows(
-    async () => {
-      const client = new InkdropClient({
-        baseUrl: "",
-        username: "",
-        password: "",
-      });
-      const notesAPI = new NotesAPI(client);
-      const notes = await notesAPI.list();
-
-      const docs = new DocsAPI(client);
-      const note: NoteDoc = await docs.get(notes[0]._id);
-      console.log(note);
-      return "";
-    },
   );
 });
